80fbdfaadd329cfdb3d5a1dd38f774236a0d91e0
Keily Salinas <keisalinas2@gmail.com>
2016-11-19 15:38:58 -0400

    added POST and GET endpoints routes

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ const port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res)=> {
-	res.send({message: `Hola ${req.params.name}`});
+app.get('/api/product', (req, res) => {
+	res.status(200).send({products: []})
+})
+
+app.get('/api/product/:id', (req, res) =>{
+})
+
+app.post('/api/product', (req, res) => {
+	console.log(req.body)
+	res.send(200, {message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:id', (req, res) => {
+
+})
+
+app.put('/api/product/:id', (req, res) => {
+
 })
 
 app.listen(port, () => {
