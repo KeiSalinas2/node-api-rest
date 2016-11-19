6ace73f7ef352279e0ef42040441e9bc5a3a2fb9
Keily Salinas <keisalinas2@gmail.com>
2016-11-19 00:31:08 -0400

    added GET endpoint using params

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express ();
-const port = process.env.port || 3001;
+const port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/hola/:name', (req, res)=> {
+	res.send({message: `Hola ${req.params.name}`});
+})
 
 app.listen(port, () => {
 	console.log(`API REST running on http://localhost:${port}`);
