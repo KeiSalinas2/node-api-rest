68e9e164bb0821da4ceeaf86b96a59c4fbd3eefd
Keily Salinas <keisalinas2@gmail.com>
2016-11-19 00:03:09 -0400

    added body-parser and nodemon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express ();
+const port = process.env.port || 3001;
 
-app.listen(3000, () => {
-	console.log('API REST running on http://localhost:3000');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log(`API REST running on http://localhost:${port}`);
 })
